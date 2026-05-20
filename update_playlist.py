c11a36f839ec28bcfa5fa8f037df8944d81beab5
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2026-05-20 21:15:56 +0300

    Update update_playlist.py

--- update_playlist.py
+++ update_playlist.py
@@ -45,7 +45,7 @@ CHANNELS = [
         "group": "Ulusal",
         "logo": "https://raw.githubusercontent.com/kadirsener1/logolar/master/logos/showtv.png",
         "tvg_id": "showtv.tr",
-        "url": "https://vavoo.to/vavoo-iptv/play/3537232978db7b25368a4b"
+        "url": "https://vavoo.to/vavoo-iptv/play/2395422638d4ff0c834c1d"
     },
     {
         "name": "CNN Türk",
